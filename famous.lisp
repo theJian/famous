e13d4811df61c533178ffd509df339fae722bc02
theJian <thejianmail@gmail.com>
2021-01-27 11:31:35 +0800

    Filter empty line

--- famous.lisp
+++ famous.lisp
@@ -1,6 +1,9 @@
-(defvar *famous* "famous.txt")
-
 (defun load-famous ()
-  (with-open-file (stream *famous*)
-    (loop for line = (read-line stream nil)
-          while line collect line)))
+  (with-open-file (stream "famous.txt")
+    (remove-if-not
+      #'(lambda (x) (> (length x) 0))
+      (loop for line = (read-line stream nil)
+            while line 
+            collect (string-trim
+                      '(#\Space #\Newline #\Backspace #\Tab #\Linefeed #\Page #\Return #\Rubout)
+                      line)))))
